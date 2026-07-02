051136a9c6cff92b99e4b18e9d5d7b7d2035e6f1
taufikcharlie <mohamadtaufik099@gmail.com>
2026-07-03 01:56:12 +0800

    feat: add BaseEntity and AuditableEntity
    Introduce mapped superclasses for automatic timestamp and auditor fields.

---
+++ cloud-storage-system/src/main/java/com/clydrive/module/AuditableEntity.java
@@ -0,0 +1,22 @@
+package com.clydrive.module;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class AuditableEntity extends BaseEntity {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
